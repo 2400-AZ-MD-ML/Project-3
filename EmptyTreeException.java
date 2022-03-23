7dcab356f74fe166647f915cc8c7ec0a86cdc1f7
AlvanZ <alvanzhuang625@gmail.com>
2022-03-22 19:40:49 -0700

    initial commit with task 1 done

---
+++ EmptyTreeException.java
@@ -0,0 +1,13 @@
+package Project3;
+public class EmptyTreeException extends RuntimeException
+{
+	public EmptyTreeException()
+	{
+		this(null);
+	} // end default constructor
+
+	public EmptyTreeException(String message)
+	{
+		super(message);
+	} // end constructor
+} // end EmptyTreeException
---
+++ TreeInterface.java
@@ -0,0 +1,9 @@
+package Project3;
+public interface TreeInterface<T>
+{
+   public T getRootData();
+   public int getHeight();
+   public int getNumberOfNodes();
+   public boolean isEmpty();
+   public void clear();
+} // end TreeInterface
